3169e57d1cdee3d818140c04d0a8c35e1d7c6e7c
Huynh Thi Ha Vy <huynhthihavy12@gmail.com>
2018-09-24 23:17:44 +0700

    Read user input

---
+++ Section_1.sh
@@ -0,0 +1,17 @@
+# Created on September 24, 2018 by Ha Vy
+# Shell scripting for Beginner: Variable & Read user input
+#! /bin/bash
+#this is a comment
+echo "Hello Bash Shell" #This is also comment
+#variable
+name="unix"
+echo "To $name from Linux"
+echo "$BASH"
+echo "This is version: $BASH_VERSION"
+echo "My direction is $PWD"
+echo "Home = $HOME"
+echo "================================"
+echo "================================"
+echo "================================"
+echo "================================"
+echo "=======Read User Input=========="
